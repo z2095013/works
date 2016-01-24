72f84d7b46ad25a5c7800481e50a2779f8612935
hearts <shin@kawadashinpei-no-MacBook.local>
2016-01-24 21:21:43 +0900

    20160124

---
+++ percentile2.py
@@ -0,0 +1,11 @@
+def percentile(N, P):
+    """
+    Find the percentile of a list of values
+
+    @parameter N - A list of values.  N must be sorted.
+    @parameter P - A float value from 0.0 to 1.0
+
+    @return - The percentile of the values.
+    """
+    n = int(round(P * len(N) + 0.5))
+    return N[n-1]
